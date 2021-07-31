3662c0dfa6313855769e718351f190d018f5d643
meglenast <58610466+meglenast@users.noreply.github.com>
2021-07-31 21:44:06 +0300

    erasing comments

--- haskell_quessing_game.hs
+++ haskell_quessing_game.hs
@@ -13,55 +13,38 @@ cat = Animal "cat" ["4-legged", "furry", "meows"]
 
 coala :: Animal
 coala = Animal "coala" ["4-legged", "furry", "lazy", "eats-bamboo"]
--- coala = Animal "coala" []
 
 dog :: Animal
 dog = Animal "dog" ["4-legged", "furry", "barks"]
--- dog = Animal "dog" []
 
 dog1 :: Animal
 dog1 = Animal "dog1" ["4-legged", "furry", "barks"]
--- dog1 = Animal "dog1" []
 
--- --TO-DO -- function that loads animals from file int a list of Animals
 dataset :: [Animal]
 dataset = [cat, coala, dog]
 
--- writeToFile :: [Animal] -> IO()
--- writeToFile [] = do putStrLn "Done writing in file..\n"
--- writeToFile (x:xs) = do writeFile "animals.txt" (show x) and writeToFile xs
-
-
--- --TO-DO -- funcition that loads the properties into list of properties
 loadPropertiesSet :: [Animal] -> [String]
 loadPropertiesSet [] = []
 loadPropertiesSet xs = foldl  (\ acc curr -> acc ++ getNewProps acc (properties curr)) [] xs
     where getNewProps curr_props new_props = filter (\ x -> not (x `elem` curr_props)) new_props
 
--- --TO-DO --ready to guess
 readyToGuess :: [Animal] -> Bool
 readyToGuess animals =  any (\ curr -> null (properties curr)) animals
 
--- --TO-DO --make a quessing
 makeGuess :: [Animal] -> String
 makeGuess animals =  animal_name $ head $ filter (\ curr -> null (properties curr)) animals
 
--- --TO-DO --reduce Animals
---when yes
 reduceProperties :: [Animal] -> String -> [Animal]
 reduceProperties [] _ = []
 reduceProperties xs has_property = map (\ curr -> (dropProp has_property curr)) xs  
 
--- --TO-DO --
---when no
 reduceAnimals :: [Animal] -> String -> [Animal]
 reduceAnimals [] _ = []
 reduceAnimals xs has_property = filter (\ x -> not(has_property `elem` (properties x))) xs
 
--- --TO-DO -- dropProp
 dropProp :: String -> Animal -> Animal
 dropProp has_property animal = Animal (animal_name animal) (dropStr has_property (properties animal))
--- --TO-DO -- dropping string cuz the one inghc is only for integers first occurence
+
 dropStr :: String -> [String] -> [String]
 dropStr _ [] = []
 dropStr str (x:xs)
@@ -87,11 +70,6 @@ ableToGuess :: [Animal] -> [Animal] -> [String] -> IO ()
 ableToGuess prevDataBase currAnimals satisfied = do
    putStrLn $ "I'll try to make a quess..\nWas your animal a/an..." ++ (makeProbableGuess currAnimals) ++ " ?"
 
---TO-DO -- ask question
--- ask :: [Animal] -> [String] -> IO ()
--- ask  animals properties = ask' animals properties []
---     where 
-
 ask :: [Animal] -> [Animal] -> [String] -> [String]  -> IO ()
 ask currAnimals animals [] satisfied = do ableToGuess currAnimals animals satisfied
 ask currAnimals [] _ satisfied = do unableToGuess currAnimals satisfied
@@ -104,14 +82,9 @@ ask currAnimals animals properties satisfied
             ask currAnimals (reduceProperties animals (head properties)) (tail properties) (head properties : satisfied)
         else 
             ask currAnimals (reduceAnimals animals (head properties)) (tail properties) satisfied
-
--- parseFile :: String -> [Animals]
--- parseFile "" = []
--- parseFile (x:xs) = 
-
+ 
 loadDataFromFile :: String -> IO String
 loadDataFromFile file = readFile file
-
 
 allNonCapitalLetters :: String -> Bool
 allNonCapitalLetters str = all (\ letter -> letter `elem` ['a'..'z']) str
