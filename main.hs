a1ef6606792be239a8a5aaef23bd9ca7e1c39a0d
meglenast <58610466+meglenast@users.noreply.github.com>
2021-08-10 19:40:34 +0300

    handling missing data knowleadge file

--- main.hs
+++ main.hs
@@ -1,13 +1,22 @@
---Animal guessing startGame
---Think of an animal and see if the game can guess your animal.
+-- Animal guessing startGame
+-- Think of an animal and see if the game can guess your animal.
 
-import AnimalGuessingGame(startGame)
+-- The file used for a knowleadge base MUST be in the following format to be considered valid:
+-- "animal_1"["prop_11",..,"prop_1i"]
+-- ..
+-- "animal_n"["prop_n1",..,"prop_nj"]
+
+import AnimalGuessingGame(startGame, file_name)
+import System.Directory
     
 main = do
-    putStrLn "Welcome to a new game of Guess the animal \n Think af an animal..\n Let's start guessing\n.."
-    putStrLn "MENU\n *Start new game(n/N)\n *Quit(q/Q)"
-    key <- getChar
-    case key of
-        'n' -> startGame "animals.txt"
-        'q' -> putStrLn "\n Thank you for playing."
-        _   -> main
+    fileExists <- doesFileExist file_name
+    if fileExists then do
+      putStrLn "Welcome to a new game of Guess the animal \n Think af an animal..\n Let's start guessing\n.."
+      putStrLn "MENU\n *Start new game(n)\n *Quit(q)"
+      key <- getChar
+      case key of
+          'n' -> startGame file_name
+          'q' -> putStrLn "\n Thank you for playing."
+          _   -> main
+    else putStrLn "Error, a file missing!\n"
